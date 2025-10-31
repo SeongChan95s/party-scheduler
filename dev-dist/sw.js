8196a0e87b8f445c37dbd7ce158cd018a0424962
박성찬 <seongchan95s@gmail.com>
2025-10-31 12:53:05 +0900

    -기본셋팅

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gtjpuf5vl8"
+    "revision": "0.6nu4ei9js8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/router.tsx
+++ src/router.tsx
@@ -1,29 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom';
-import HomePage from '@/pages/HomePage';
-import AboutPage from '@/pages/AboutPage';
-import NotFoundPage from '@/pages/NotFoundPage';
-import UsersPage from '@/pages/user/UsersPage';
-import UserDetailPage from '@/pages/user/UserDetailPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import UsersPage from './pages/user/UsersPage';
+import UserDetailPage from './pages/user/UserDetailPage';
+import NotFoundPage from './pages/NotFoundPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/about',
-    element: <AboutPage />,
-  },
-  {
-    path: '/user',
-    element: <UsersPage />,
-  },
-  {
-    path: '/user/:id',
-    element: <UserDetailPage />,
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomePage />
+	},
+	{
+		path: '/about',
+		element: <AboutPage />
+	},
+	{
+		path: '/user',
+		element: <UsersPage />
+	},
+	{
+		path: '/user/:id',
+		element: <UserDetailPage />
+	},
+	{
+		path: '*',
+		element: <NotFoundPage />
+	}
 ]);
+
+export default function Router() {
+	return <RouterProvider router={router} />;
+}
